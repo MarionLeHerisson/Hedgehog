362e8409e6e6010b60dcd763f63795caf5ce138b
MarionLeHerisson <marion.hurteau1@gmail.com>
2017-10-01 15:04:17 +1300

    footer open links in new tab

--- Application/View/basics/footer.php
+++ Application/View/basics/footer.php
@@ -5,10 +5,10 @@
         <footer class="col-md-12 text-center">
             <hr class="featurette-divider">
             © Marion Hurteau 2017 &bull;
-            <a href="https://github.com/MarionLeHerisson">GitHub</a> &bull;
-            <a href="https://www.instagram.com/marionleherisson/">Instagram</a> &bull;
-            <a href="https://www.facebook.com/marionestherhurteau">Facebook</a> &bull;
-            <a href="https://www.linkedin.com/in/marion-hurteau-07290587/">LinkedIn</a>
+            <a href="https://github.com/MarionLeHerisson" target="_blank">GitHub</a> &bull;
+            <a href="https://www.instagram.com/marionleherisson/" target="_blank">Instagram</a> &bull;
+            <a href="https://www.facebook.com/marionestherhurteau" target="_blank">Facebook</a> &bull;
+            <a href="https://www.linkedin.com/in/marion-hurteau-07290587/" target="_blank">LinkedIn</a>
             <div class="col-md-12">
                 <?php echo $footer; ?>
             </div>
