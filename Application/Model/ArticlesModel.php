3fa07ffb8a53ad96aa13794194002cd5f02bd78b
MarionLeHerisson <marion.hurteau1@gmail.com>
2017-11-28 12:15:23 +1300

    article edition

--- Application/Model/ArticlesModel.php
+++ Application/Model/ArticlesModel.php
@@ -47,11 +47,12 @@ class ArticlesModel extends DefaultModel {
      *
      * @param array $data
      * @return PDOStatement
+     * @todo update url :')'
      */
     public function updateArticle($data) {
         $db = $this->connectDb();
         $query = $db->prepare("UPDATE " . $this->_name . " " .
-                                "SET article_type_id = ?, theme_id = ?, title = ?, intro = ?, content = ?, url = ?, status_id = ?, updated_at = NOW() " .
+                                "SET article_type_id = ?, theme_id = ?, title = ?, intro = ?, content = ?, status_id = ?, updated_at = NOW() " .
                                 "WHERE id = ?;");
         $query->execute(
             [
@@ -60,7 +61,6 @@ class ArticlesModel extends DefaultModel {
                 $data['title'],
                 $data['editor_intro'],
                 $data['editor_content'],
-                $data['url'],
                 $data['status'],
                 $data['article_id']
             ]
@@ -106,11 +106,11 @@ class ArticlesModel extends DefaultModel {
      */
     public function getArticle($id) {
         $db = $this->connectDb();
-        $query = $db->prepare("SELECT a.id, article_type_id, theme_id, author_id, title, intro, content, url, status_id, created_at " .
-                                ",m.id as main_media_id, m.is_main " .
+        $query = $db->prepare("SELECT a.id, article_type_id, theme_id, author_id, title, intro, content, status_id, created_at, url_id " .
+                                ", u.url " .
                                 "FROM " . $this->_name . " AS a " .
-                                "LEFT JOIN medias AS m " .
-                                "ON m.article_id = a.id " .
+                                "LEFT JOIN urls AS u " .
+                                "ON a.url_id = u.id " .
                                 "WHERE a.id = ?;");
         $query->execute([$id]);
         return $query;
