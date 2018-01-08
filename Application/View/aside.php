03f74f9955869e8964ecb6d5ccbc7e14d0072ddb
MarionLeHerisson <marion.hurteau1@gmail.com>
2018-01-08 16:50:09 +1300

    instagram tests

--- Application/View/aside.php
+++ Application/View/aside.php
@@ -29,6 +29,18 @@
   </div>
 
   <?php if(DEBUG == 1): ?>
+
+  <div class="sidebar-module sidebar-module-inset">
+    <h4>D'autres photos sont sur Instagram !</h4>
+   <!-- InstaWidget -->
+    <a href="https://instawidget.net/v/user/marionleherisson" id="link-a3639f98d1d200b0a5c33d71b512f4da5ce3b8d885ed2ca2bb79f23c0051f8ba">@marionleherisson</a>
+    <script src="https://instawidget.net/js/instawidget.js?u=a3639f98d1d200b0a5c33d71b512f4da5ce3b8d885ed2ca2bb79f23c0051f8ba&width=223px"></script>
+
+    <h4>Ou alors</h4>
+    <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BbYyLdVDhml/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div><p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BbYyLdVDhml/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">Une publication partagée par Marion (@marionleherisson)</a> le <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2017-11-12T07:44:08+00:00">11 Nov. 2017 à 11 :44 PST</time></p></div></blockquote>
+<script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
+  </div>
+
   <div class="sidebar-module">
     <h4>Archives</h4>
     <ol class="list-unstyled">
