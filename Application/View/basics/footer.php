3165efd5181a0a64ced89ec586f9664bd67c4e72
MarionLeHerisson <marion.hurteau1@gmail.com>
2018-06-24 11:20:34 +0100

    commented Instagram in footer

--- Application/View/basics/footer.php
+++ Application/View/basics/footer.php
@@ -2,15 +2,17 @@
                 <meta http-equiv="refresh" content="1; URL=nojs.php">
             </noscript>
         </div> <!-- .page_content -->
+
         <footer class="col-md-12 text-center">
             <hr class="featurette-divider">
             © Marion Hurteau 2018 &bull;
             <a href="https://github.com/MarionLeHerisson" target="_blank">GitHub</a> &bull;
-            <a href="https://www.instagram.com/marionleherisson/" target="_blank">Instagram</a> &bull;
+            <!-- <a href="https://www.instagram.com/marionleherisson/" target="_blank">Instagram</a> &bull; -->
             <a href="https://www.linkedin.com/in/marion-hurteau-07290587/" target="_blank">LinkedIn</a>
             <div class="col-md-12">
                 <?php echo $footer; ?>
             </div>
         </footer>
+        
     </body>
 </html>
